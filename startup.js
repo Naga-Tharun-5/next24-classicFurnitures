20faf45b61e7a60e01ea14ab13fe3ddfcad4a708
Naga-Tharun-5 <141549352+Naga-Tharun-5@users.noreply.github.com>
2024-08-19 21:42:49 +0530

    Add files via upload

---
+++ startup.js
@@ -0,0 +1,128 @@
+function WoodenCouchbackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let woodenCouchContainer = document.getElementById("woodenCouchContainer");
+    mainContainer.classList.remove("d-none");
+    woodenCouchContainer.classList.remove("d-block");
+    woodenCouchContainer.classList.add("d-none");
+}
+
+function windowsBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.remove("d-none");
+    let windowContainer = document.getElementById("windowsContainer");
+    windowContainer.classList.remove("d-block");
+    windowContainer.classList.add("d-none");
+}
+
+function cupboardsBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.remove("d-none");
+    let cupboardContainer = document.getElementById("cupboardsContainer");
+    cupboardContainer.classList.remove("d-block");
+    cupboardContainer.classList.add("d-none");
+}
+
+function doorsBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.remove("d-none");
+    let doorsContainer = document.getElementById("doorsContainer");
+    doorsContainer.classList.remove("d-block");
+    doorsContainer.classList.add("d-none");
+}
+
+function dressingTableBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let dressingTableCntainer = document.getElementById("dressingTableContainer");
+    dressingTableCntainer.classList.remove("d-block");
+    dressingTableCntainer.classList.add("d-none");
+    mainContainer.classList.remove("d-none");
+}
+
+function bedBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let bedContainer = document.getElementById("bedContainer");
+    mainContainer.classList.remove("d-none");
+    bedContainer.classList.remove("d-block");
+    bedContainer.classList.add("d-none");
+}
+
+function sofaBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let sofaContainer = document.getElementById("sofaContainer");
+    sofaContainer.classList.add("d-none");
+    sofaContainer.classList.remove("d-block");
+    mainContainer.classList.remove("d-none");
+}
+
+function dinningBackOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let dinningContainer = document.getElementById("dinningTableContainer");
+    mainContainer.classList.remove("d-none");
+    dinningContainer.classList.remove("d-block");
+    dinningContainer.classList.add("d-none");
+}
+
+function woodenCouchOnclick() {
+    let woodenCouchContainer = document.getElementById("woodenCouchContainer");
+    let mainContainer = document.getElementById("mainContaier");
+    woodenCouchContainer.classList.remove("d-none");
+    woodenCouchContainer.classList.add("d-block");
+    mainContainer.classList.add("d-none");
+}
+
+function windowsContainerOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.add("d-none");
+    let windowContainer = document.getElementById("windowsContainer");
+    windowContainer.classList.remove("d-none");
+    windowContainer.classList.add("d-block");
+}
+
+function cupboardsContainerOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.add("d-none");
+    let cupboardContainer = document.getElementById("cupboardsContainer");
+    cupboardContainer.classList.add("d-block");
+    cupboardContainer.classList.remove("d-none");
+}
+
+function bedOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let bedContainer = document.getElementById("bedContainer");
+    bedContainer.classList.remove("d-none");
+    bedContainer.classList.add("d-block");
+    mainContainer.classList.add("d-none");
+
+}
+
+function sofaOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.add("d-none");
+    let sofaContainer = document.getElementById("sofaContainer");
+    sofaContainer.classList.remove("d-none");
+    sofaContainer.classList.add("d-block");
+}
+
+function doorsOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.add("d-none");
+    let doorsContainer = document.getElementById("doorsContainer");
+    doorsContainer.classList.remove("d-none");
+    doorsContainer.classList.add("d-block");
+}
+
+function dressingTableOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    mainContainer.classList.add("d-none");
+    let dressingTableCntainer = document.getElementById("dressingTableContainer");
+    dressingTableCntainer.classList.remove("d-none");
+    dressingTableCntainer.classList.add("d-block");
+}
+
+function dinningTableOnclick() {
+    let mainContainer = document.getElementById("mainContaier");
+    let dinningContainer = document.getElementById("dinningTableContainer");
+    dinningContainer.classList.remove("d-none");
+    dinningContainer.classList.add("d-block");
+    mainContainer.classList.add("d-none");
+}
